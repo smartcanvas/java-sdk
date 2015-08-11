58ff3f5600d9487295824172f0530c51badedb93
fabito <fuechi@ciandt.com>
2015-08-11 14:05:43 -0300

    sanity tests for moderation approval

--- src/test/java/com/smartcanvas/CardModerationIntegrationTests.java
+++ src/test/java/com/smartcanvas/CardModerationIntegrationTests.java
@@ -11,9 +11,9 @@ import org.junit.Test;
 import com.smartcanvas.model.Card;
 import com.smartcanvas.model.CardId;
 
-public class CardModerationTests extends AbstractSmartCanvasIntegrationTests{
+public class CardModerationIntegrationTests extends AbstractSmartCanvasIntegrationTests{
 
-    public CardModerationTests() throws JoseException {
+    public CardModerationIntegrationTests() throws JoseException {
         super();
     }
 
@@ -21,7 +21,7 @@ public class CardModerationTests extends AbstractSmartCanvasIntegrationTests{
     public void test() throws IOException {
        Card c = Card.newBuilder()
                .withContentProvider("Acme", null, UUID.randomUUID().toString())
-               .withTitle("My super Card").build();
+               .withTitle(UUID.randomUUID().toString()).build();
        CardId id = smartcanvas.cards().insert(c);
        smartcanvas.moderations().approve(id);
     }
