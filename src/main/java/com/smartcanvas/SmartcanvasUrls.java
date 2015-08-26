1302a9a03053623c6f55f806e96d31283a892442
ltung <ltung@ciandt.com>
2015-08-26 15:12:45 -0300

    Use PRD API url instead of DEV url

--- src/main/java/com/smartcanvas/SmartcanvasUrls.java
+++ src/main/java/com/smartcanvas/SmartcanvasUrls.java
@@ -4,10 +4,10 @@ import com.google.api.client.http.GenericUrl;
 
 public class SmartcanvasUrls {
 
-    private static final String DEFAULT_ROOT_URL = "https://api-sandbox.smartcanvas.com/";
+    private static final String DEFAULT_ROOT_URL = "https://api.smartcanvas.com/";
 
 	public static class CardApiUrl extends GenericUrl {
-        
+
     	public static final String DEFAULT_SERVICE_PATH = "card/v1";
         final static String url = "%s%s/cards";
         final static String urlUpdate = "%s%s/cards/%s";
@@ -16,39 +16,39 @@ public class SmartcanvasUrls {
             super(String.format(url, DEFAULT_ROOT_URL, DEFAULT_SERVICE_PATH));
         }
 
-        public CardApiUrl(String directUrl) {
+        public CardApiUrl(final String directUrl) {
             super(String.format(url, directUrl, DEFAULT_SERVICE_PATH));
          }
 
-        public CardApiUrl(String directUrl, boolean useDirectUrl) {
+        public CardApiUrl(final String directUrl, final boolean useDirectUrl) {
             super(String.format(url, directUrl, DEFAULT_SERVICE_PATH));
         }
 
-        public CardApiUrl(String put, String id) {
+        public CardApiUrl(final String put, final String id) {
         	super(String.format(urlUpdate, DEFAULT_ROOT_URL, DEFAULT_SERVICE_PATH, id));
         }
 
-        public CardApiUrl(String put, String id, String directUrl) {
+        public CardApiUrl(final String put, final String id, final String directUrl) {
             super(String.format(urlUpdate, directUrl, DEFAULT_SERVICE_PATH, id));
         }
     }
 
     public static class CardModerationUrl extends GenericUrl {
-        
+
         public static final String DEFAULT_SERVICE_PATH = "card/v1";
         final static String urlUpdate = "%s%s/cards/%s/moderations";
 
-        public CardModerationUrl(String cardId) {
+        public CardModerationUrl(final String cardId) {
             super(String.format(urlUpdate, DEFAULT_ROOT_URL, DEFAULT_SERVICE_PATH, cardId));
             System.out.println(DEFAULT_ROOT_URL);
         }
 
-        public CardModerationUrl(String directUrl, String cardId) {
+        public CardModerationUrl(final String directUrl, final String cardId) {
             super(String.format(urlUpdate, directUrl, DEFAULT_SERVICE_PATH, cardId));
             System.out.println(directUrl);
         }
 
-        public static CardModerationUrl of(String directUrl, Long cardId) {
+        public static CardModerationUrl of(final String directUrl, final Long cardId) {
             return new CardModerationUrl(directUrl, String.valueOf(cardId));
         }
 
